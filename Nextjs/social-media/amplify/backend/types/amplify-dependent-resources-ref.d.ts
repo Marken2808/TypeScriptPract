c9366e0d8d627a034358fb272fe9801e887ff1ae
Marken <nctuan.ityu@gmail.com>
2022-03-18 23:13:59 +0000

    update

--- Nextjs/social-media/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "socialmedia": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
